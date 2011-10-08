6ec4cbad06b1ad71dbcc19d0dbe9edccfcb01756
Vineet <vineetk32@gmail.com>
2011-10-08 16:47:48 -0400

    Added OpenMP and time

--- MP1/gauss.c
+++ MP1/gauss.c
@@ -7,6 +7,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
+#include <sys/time.h>
 
 #define MATRIX_DIM 2000
 #define SEED 0
@@ -17,68 +19,74 @@ double Y[MATRIX_DIM];
 
 void printMatrix() 
 {
-  int i, j;
-  for (i=0; i<MATRIX_DIM; i++) {
-    for (j=0; j<MATRIX_DIM; j++) {
-      printf("%7.3f ", A[i][j]);
-    }
-    printf(" | %7.3f \n", Y[i]);
-  }
+	int i, j;
+	for (i=0; i<MATRIX_DIM; i++) {
+		for (j=0; j<MATRIX_DIM; j++) {
+			printf("%7.3f ", A[i][j]);
+		}
+		printf(" | %7.3f \n", Y[i]);
+	}
 }
 
 void printSol() 
 {
-  int i, j;
-  printf("--- Solution of Y=AX: \n");
-  for (i=0; i<MATRIX_DIM; i++) {
-    printf(" X%d = %7.3f \n", i, X[i]);
-  }
+	int i, j;
+	printf("--- Solution of Y=AX: \n");
+	for (i=0; i<MATRIX_DIM; i++) {
+		printf(" X%d = %7.3f \n", i, X[i]);
+	}
 }
 
 initMatrix()
 {
-  int i, j;
+	int i, j;
 
-  srand(SEED);
-  for (i=0; i<MATRIX_DIM; i++) {
-    for (j=0; j<MATRIX_DIM; j++) {
-      A[i][j] =  100 * ((double)rand() / ((double)(RAND_MAX)+(double)(1)) ) ;      
-    }
-    Y[i] = 100 * ((double)rand() / ((double)(RAND_MAX)+(double)(1)) ) ;      
-  }
+	srand(SEED);
+	for (i=0; i<MATRIX_DIM; i++) {
+		for (j=0; j<MATRIX_DIM; j++) {
+			A[i][j] =  100 * ((double)rand() / ((double)(RAND_MAX)+(double)(1)) ) ;
+		}
+		Y[i] = 100 * ((double)rand() / ((double)(RAND_MAX)+(double)(1)) ) ;
+	}
 }
 
 int main() 
 {
-  double adjust;
-  int i, j, pivot;
+	double adjust;
+	int i, j, pivot;
+	struct timeval start_time,end_time;
 
-  // initialize matrix
-  initMatrix();
+	gettimeofday(&start_time,0);
+	// initialize matrix
+	initMatrix();
 
-  // upper triangulation step
-  for (pivot=0; pivot<MATRIX_DIM-1; pivot++) {
-    for (i=pivot+1; i<MATRIX_DIM; i++) {
-      adjust = A[i][pivot] / A[pivot][pivot];
-      for (j=pivot; j<MATRIX_DIM; j++) {
-        A[i][j] = A[i][j] - adjust * A[pivot][j];   
-      }
-      Y[i] = Y[i] - adjust * Y[pivot];
-    }
-  }
+	// upper triangulation step
+#pragma omp parallel for private(pivot,i,j) shared(A,Y)
+	for (pivot=0; pivot<MATRIX_DIM-1; pivot++) {
+		for (i=pivot+1; i<MATRIX_DIM; i++) {
+			adjust = A[i][pivot] / A[pivot][pivot];
+#pragma omp parallel for private(j) shared(adjust,i,A) schedule(static)
+			for (j=pivot; j<MATRIX_DIM; j++) {
+				A[i][j] = A[i][j] - adjust * A[pivot][j];
+			}
+			Y[i] = Y[i] - adjust * Y[pivot];
+		}
+	}
 
-  // diagonalization step
-  for (pivot=MATRIX_DIM-1; pivot>0; pivot--) {
-    for (i=pivot-1; i>=0; i--) {
-        adjust = A[i][pivot] / A[pivot][pivot];
-      A[i][pivot] = A[i][pivot] - adjust * A[pivot][pivot];   
-      Y[i] = Y[i] - adjust * Y[pivot];
-    }
-  }
+	// diagonalization step
+	for (pivot=MATRIX_DIM-1; pivot>0; pivot--) {
+		for (i=pivot-1; i>=0; i--) {
+			adjust = A[i][pivot] / A[pivot][pivot];
+			A[i][pivot] = A[i][pivot] - adjust * A[pivot][pivot];
+			Y[i] = Y[i] - adjust * Y[pivot];
+		}
+	}
 
-  // solution step
-  for (i=0; i<MATRIX_DIM; i++)
-    X[i] = Y[i] / A[i][i];
-  
-  printSol();
+	// solution step
+	for (i=0; i<MATRIX_DIM; i++)
+		X[i] = Y[i] / A[i][i];
+
+	//printSol();
+	gettimeofday(&end_time,0);
+	printf("\nTotal execution time for parallel gauss- %lf ms\n",(end_time.tv_usec - start_time.tv_usec));
 }
