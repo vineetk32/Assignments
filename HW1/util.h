a3aadbc0fa47fc328116064ee84a730a149bcbeb
Vineet <vineetk32@gmail.com>
2012-02-06 14:16:19 -0500

    Fixing the tree

--- HW1/util.h
+++
@@ -1,9 +0,0 @@
-#include "string.h"
-
-#ifndef __UTIL_H
-#define __UTIL_H
-
-int splitLine(const char *in, char **out,const char *delim);
-int arrayContains(const char **array, const char *element, int arrayLen);
-
-#endif
