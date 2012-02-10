c064da310a382c1c61487d4431c264b812023c54
Vineet <vineetk32@gmail.com>
2012-02-09 21:05:28 -0500

    Replaced shaunaks ugly code

--- HW2/p3.c
+++ HW2/p3.c
@@ -1,192 +1,334 @@
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include "util.h"
+#include <pthread.h>
 
-#include <sys/time.h>
+#define MAX_THREADS 20
+#define NUM_THREADS 8
+#define MAX_PROCESSES 12
 
-#define MAX_THREAD_NUM 300
-#define MAX_TEXTSIZE (1<<20)
-#define MAX_LINE_SIZE 32
-#define MAX_PROC 10
-
-char* proc_list[MAX_PROC]; //share the names of the process between the threads
-int results[MAX_THREAD_NUM][MAX_PROC]; // write the results in this array
-struct th_arg_s
+struct threadPackage_t
 {
-    char *buf;
-    int start;
-    int end;
-    int thread_id;
-    int count;
+	long startOffset;
+	long endOffset;
+	char **processList;
+	int numProcesses;
+	int *processCount;
+	char *buffer;
 };
-void* string_match(void* arg)
+
+void *threadFunc(void *threadPackage)
 {
-    struct th_arg_s *th_arg = (struct th_arg_s*)(arg);
-    //find the start of the line from which one has to read
-    int start=th_arg->start;
-    int end=0;
-    int start_ch =th_arg->buf[th_arg->start];
-    while (start_ch!='\n' && start_ch != EOF && (start-th_arg->start)<=th_arg->count && start!=0)
-        //check if the first character is newline or eof
-        //also verrify that the start character is in range of start + count that was supplied by the main program
-    {
-        //if(th_arg->buf[(th_arg->start)-1]=='\n') //if the block was divided in such a way that block starts from a new line
-        //    break;
-        start+=1;;
-        start_ch =th_arg->buf[start];
+	char **splitBuff;
+	int i,numTokens;
 
-    }
-    if (start!=th_arg->start)
-        start+=1;
-    //we have the start position of the new line
-    int lo_in=th_arg->start+th_arg->count;
-    while (th_arg->buf[lo_in]!='\n')
-    {
-        lo_in=lo_in+1;
-    }
-    end=lo_in;
+	struct threadPackage_t *package;
+	char *tempBuff;
 
-    //now we have the start and end position of the block that we want to analyze
-    int line_start=start;
-    if (line_start>=end)
-    {
-        //if it goes beyond its boundary then exit the thread
-        pthread_exit((void*) arg);
-    }
-    while (1)
-    {
-        int lo_in2;
+	long start,end,curr;
+	int exitFlag = 0;
 
-        for (lo_in2=0;lo_in2<10 && (proc_list[lo_in2]!=0);lo_in2++)
-        {
-            if (!strncmp(&(th_arg->buf[line_start+16]),proc_list[lo_in2],strlen(proc_list[lo_in2])))
-            {
-                results[th_arg->thread_id][lo_in2]++;
-            }
-        }
-        while (line_start<end && th_arg->buf[line_start]!='\n' && th_arg->buf[line_start]!=EOF)
-        {
-            line_start++;
-        }
-        line_start++;//to adjust the pointer to the next line
-        if (line_start>=end)
-            break;
-    }
-    pthread_exit((void*) arg);
+	package = (struct threadPackage_t *) threadPackage; 
+
+	start = package->startOffset;
+	end = package->endOffset;
+
+
+	#ifdef __DEBUG
+	printf("\nstart: %ld end: %ld curr: %ld - ",start,end,curr);
+	#endif
+
+	//Allocate the array which will be used to hold each individual word in a line.
+	splitBuff = (char **) malloc ( MAX_WORDS * sizeof(char *));
+	tempBuff = (char *) malloc ( BUFFER_SIZE * sizeof(char));
+
+	for(i = 0; i < MAX_WORDS;i++)
+	{
+		splitBuff[i] = (char *) malloc ( MAX_WORD_LENGTH * sizeof(char));
+	}
+
+	//Read the input data between the offsets char-by-char
+	while (exitFlag == 0)
+	{
+		/*while (package->buffer[start] != '\n')
+		{
+			start++;
+			if (package->buffer[start] == '\0')
+			{
+				exitFlag = 1;
+				break;
+			}
+		}*/
+		start++;
+		curr = start + 1;
+		if (end != -1)
+		{
+			while (package->buffer[curr] != '\n' && curr < end)
+			{
+				curr++;
+			}
+
+			if (curr >= end)
+			{
+				exitFlag = 1;
+			}
+			strncpy(tempBuff,(package->buffer) + start,curr-start);
+			curr++;
+		}
+		else
+		{
+			while (package->buffer[curr] != '\n' && package->buffer[curr] != '\0')
+			{
+				curr++;
+			}
+
+			if (package->buffer[curr] == '\0')
+			{
+				exitFlag = 1;
+			}
+			strncpy(tempBuff,(package->buffer)+ start,curr-start);
+		}
+		#ifdef __DEBUG
+		printf("\nReading %d --> %d (%s)",start,curr,tempBuff);
+		#endif
+		tempBuff[curr - start - 1] = '\0';
+		start = curr;
+		if (strlen(tempBuff) == 0)
+		{
+			#ifdef __DEBUG
+			printf("\nZero-len tempBuff! start - %ld curr - %ld end - %ld",start,curr,end);
+			#endif
+		}
+		else
+		{
+			package->processCount[package->numProcesses]++;
+		}
+
+		#ifdef __DEBUG
+		//printf("\nTempBuff - %s",tempBuff);
+		#endif
+	
+		//TODO: Apparently, strtok_r is thread-safe only for dynamic mem. Check
+		numTokens = splitLine(tempBuff,splitBuff," :[]");
+		
+		//Check if the current logLine is written by a process we're interested in.
+		if (numTokens > 5)
+		{
+			if ( (i = arrayContains(package->processList,splitBuff[5],package->numProcesses)) > 0)
+			{
+				package->processCount[i]++;
+			}
+			//for (i = 0; i < numTokens; i++)
+			//{
+			//	printf("\t%d: %s",i,splitBuff[i]);
+			//}
+			//(package->*totalLines)++;
+		}
+		else
+		{
+			#ifdef __DEBUG
+			printf("\nBad Line - %s",tempBuff);
+			#endif
+		}
+	}
+	for (i = 0; i < package->numProcesses; i++)
+	{
+		free(splitBuff[i]);
+	}
+	free(splitBuff);
 }
-int main(int argc, char** argv)
+
+int adjustThreadOffsets(char *fileContents,int fileSize,long *offsetArray,int numThreads)
 {
-    pthread_t thread[MAX_THREAD_NUM];
-    int num[MAX_THREAD_NUM];
-    int final_count[10]={0},proc_lcount=0;
-    struct th_arg_s th_arg[MAX_THREAD_NUM];
-    pthread_attr_t attr;
-    int rc,t,ptr,log_lines,count;
-    struct th_arg_s *status;
-    char *buf;
-    struct timeval p[MAX_THREAD_NUM],q[MAX_THREAD_NUM];
-    int textsize,blocksize;
+	int i,blockSize;
+	long ptr = 0;
 
-    //check if the proper inputs ahve been given
-    if (argc <3)
-    {
-        printf("There needs to be two arguments \n");
-        exit(1);
-    }
+	blockSize = fileSize / numThreads;
+	offsetArray[0] = 0;
+	
+	//Adjust the offsets so that each thread gets atleast blockSize + EOL to process.
+	for (i = 1; i < numThreads; i++)
+	{
+		//If previous offset is set to EOF, all further offsets must also be set to EOF.
+		if (offsetArray[i - 1] != -1)
+		{
+			//fseek(flog,blockSize,offsetArray[i - 1]);
+			ptr += blockSize;
+		}
+		else
+		{
+			offsetArray[i] = -1;
+		}
+		while ( fileContents[ptr] != '\n' && fileContents[ptr] != '\0')
+		{
+			ptr++;
+		}
+		
+		if ( fileContents[ptr] == '\n')
+		{
+			offsetArray[i] = ptr;
+		}
+		else if (fileContents[ptr] == '\0')
+		{
+			offsetArray[i] = -1;
+		}
+	}
 
-    //open both the files and see if they were opened successfully
-    FILE *fp_log=fopen(argv[1],"r");
-    FILE *fp_proc=fopen(argv[2],"r");
-    if (fp_log==NULL || fp_proc==NULL)
-    {
-        printf("error opening the file \n");
-        exit(1);
-    }
+	#ifdef __DEBUG
+	printf("\noffsetArray - ");
+	for (i = 0; i < numThreads; i++)
+	{
+		printf(" %d: %ld",i,offsetArray[i]);
+	}
+	#endif
+}
 
-    //copy the data of the log file in a buffer
-    if ( (buf = (char*)malloc(MAX_TEXTSIZE)) == NULL )
-        exit(-1);
-
-    for ( ptr = 0 ; !feof(fp_log) ; )
-        ptr += fread(&(buf[ptr]), 1, 1, fp_log);
-
-    textsize = ptr;
-    //printf("the textsize is %d and the text is %s\n",textsize,buf);
-
-    //parse the process names from the file and store them in an array
-    int lo=0; //loop variable
-    while (!feof(fp_proc) && lo<10)
-    {
-        proc_list[lo]=malloc(sizeof(char)*20);
-        fscanf(fp_proc,"%s\n",proc_list[lo]);
-        //printf("the read value was %s at %d \n",*(proc_list+lo),lo);
-        lo++;
-    }
-    //
+int main(int argc, char **argv)
+{
+	FILE *flog,*fproc;
+	char tempBuff[BUFFER_SIZE] = {'\0'};
+	int i = 0,j = 0,k = 0,numProcesses,totalLines = 0,fileSize;
+	char *processList[MAX_PROCESSES];
+	int processCount[MAX_THREADS][MAX_PROCESSES];
+	long offsetArray[MAX_THREADS];
+	struct timespec start_time,end_time;
+	pthread_t threads[MAX_THREADS];
+	char *fileContents;
+	struct threadPackage_t threadPackage[MAX_THREADS];
+	int printedOnce = 0,numThreads;
 
 
-    /* Initialize and set thread detached attribute */
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-    //count=25;
-    for (count=10;count<=MAX_THREAD_NUM;count=count+5)
-    {
-        gettimeofday(&p[count%10], NULL);
+	if (argc != 3)
+	{
+		printf("Incorrect usage!\nUsage - %s path-to-log-file path-to-process-list-file.\n",argv[0]);
+		return -1;
+	}
 
-        for (t=0; t<count; t++)
-        {
-            th_arg[t].end=textsize;
-            th_arg[t].start= t>0 ? (th_arg[t-1].start+th_arg[t-1].count):0;
-            th_arg[t].buf=buf;
-            th_arg[t].count=(textsize/count)+1;
-            th_arg[t].thread_id=t;
-            for (lo=0;lo<10;lo++)
-            {
-                results[t][lo]=0;
-            }
+	clock_gettime(CLOCK_MONOTONIC,&start_time);
 
-            rc = pthread_create(&thread[t], &attr, string_match, (void *)&th_arg[t]);
-            if (rc!=0)
-            {
-                perror("thread_create:");
-            }
-        }
-        /* Free attribute and wait for the other threads */
-        pthread_attr_destroy(&attr);
-        proc_lcount=0;
-        for(t=0;t<10;t++)
-        {   final_count[t]=0; //reset the final count
-            }
-        for (t=0; t<count; t++)
-        {
-            rc = pthread_join(thread[t], (void**)&status);
+	fproc = fopen(argv[2],"r");
+	if (fproc == NULL)
+	{
+		printf("\nError opening processfile!\n");
+		return -1;
+	}
+	
+	//Read every processName from the processes file
+	i = 0;
+	while (fscanf(fproc,"%s",tempBuff) >= 0)
+	{
+		processList[i] = strdup(tempBuff);
+		i++;
+	}
+	fclose(fproc);
+	numProcesses = i;
+	
+	flog = fopen(argv[1],"r");
+	if (flog == NULL)
+	{
+		printf("\nError opening logfile!\n");
+		return -1;
+	}
+	//Get the total byte size of the file.
+	if (fseek(flog,0,SEEK_END) == 0)
+	{
+		fileSize = ftell(flog);
+	}
 
-            int no_comm=0;
+	fileContents = (char *) malloc (sizeof(char) * fileSize);
+	rewind(flog);
+	fread(fileContents,1,fileSize,flog);
+	fclose(flog);
 
-            while (1)
-            {
-                final_count[no_comm]=final_count[no_comm]+results[t][no_comm];
-                proc_lcount=proc_lcount+results[t][no_comm];
-                no_comm++;
-                if (no_comm>10 || proc_list[no_comm]==0)
-                    break;
-            }
-            if (rc!=0)
-            {
-                perror("thread_join:");
-                exit(-1);
-            }
-        }
-        gettimeofday(&q[count%10], NULL);
-        log_lines=0;
-        printf("%d \t %d \t %d \t %8ld\n",(textsize/count),count,proc_lcount,q[count%10].tv_usec - p[count%10].tv_usec + (q[count%10].tv_sec-p[count%10].tv_sec)*1000000);
-    }
-    for (t=0;t<10 && proc_list[t]!=0;t++)
-        {
-            printf("pName: %s count: %d \n",proc_list[t],final_count[t]);
-            log_lines+=final_count[t];
-        }
-    printf("The total log lines are %d \n",log_lines);
+	for (k = 1; (k * 2) <  16; k++)
+	{
+		numThreads = k * 2;
+		for (i = 0; i <= numThreads; i++)
+		{
+			for (j = 0; j < MAX_PROCESSES; j++)
+			{
+				processCount[i][j] = 0;
+			}
+		}
+
+		adjustThreadOffsets(fileContents,fileSize,offsetArray,numThreads);
+
+		//Get the linecount
+		/*while ( fgets(tempBuff,1024,flog) > 0)
+		{
+			totalLines++;
+		}
+		threadLines = totalLines / NUM_THREADS;
+		offsetArray[0] = 0;
+		for ( i = 1; i < NUM_THREADS; i++)
+		{
+			offsetArray[i] = offsetArray[i - 1] + threadLines;
+		}*/
+
+		for (i = 0 ;i < numThreads; i++)
+		{
+			threadPackage[i].processCount = processCount[i];
+			threadPackage[i].processList = processList;
+			threadPackage[i].numProcesses = numProcesses;
+			threadPackage[i].buffer = fileContents;
+			threadPackage[i].startOffset = offsetArray[i];
+
+			if (i < (numThreads - 1))
+			{
+				threadPackage[i].endOffset = offsetArray[i+1] - 1;
+			}
+			else
+			{
+				threadPackage[i].endOffset = -1;
+			}
+
+			#ifdef __DEBUG
+			printf("\nThreadPackage[%d] - \n",i);
+			printf("startOffset: %ld endOffset: %ld\n",threadPackage[i].startOffset,threadPackage[i].endOffset);
+			#endif
+			pthread_create(&threads[i],NULL,&threadFunc,&threadPackage[i]);
+		}
+
+		for (i = 0 ;i < numThreads; i++)
+		{
+			if (pthread_join(threads[i],NULL) != 0)
+			{
+				printf("\nERROR: pthread_join returned non-zero!\n");
+			}
+		}
+
+		clock_gettime(CLOCK_MONOTONIC,&end_time);
+		//blockSize: numOfThreads: totalCount: runningTime: 
+
+		//Now collate all the process counts received from each threads into one.
+		for (i = 0 ; i < numThreads; i++)
+		{
+			for (j = 0; j < MAX_PROCESSES; j++)
+			{
+				processCount[numThreads][j] += processCount[i][j];
+			}
+			totalLines += processCount[i][numProcesses];
+		}
+
+		if (printedOnce == 0)
+		{
+			for(i = 0; i < numProcesses;i++)
+			{
+				printf("\npName:%s count:%d",processList[i],processCount[numThreads][i]);
+			}
+			printf("\nTotal Number of loglines: %d\n",totalLines);
+			printedOnce = 1;
+		}
+		//printf("\nNow %d\n",numThreads);
+		printf("\n%d: %d: %d: %lfms: \n",fileSize/numThreads,numThreads,totalLines,(end_time.tv_sec - start_time.tv_sec) +  (end_time.tv_nsec - start_time.tv_nsec)/1000000.0);
+	}
+	//Be a good boy and release all the memory
+	for (i = 0; i < numProcesses; i++)
+	{
+		free(processList[i]);
+	}
+	return 0;
 
 }
